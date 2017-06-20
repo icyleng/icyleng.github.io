6bc410bd373a8a1860a01f7ddb0945f3affeb6cc
icyleng <jleng@student.otis.edu>
2017-06-19 17:28:23 -0700

    home

---
+++ js/index.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+    $(".catalog")
+     .mouseover(function(){
+        $(".catalog-element1").css({"line-height":"20px", "color":"white", "transform":"scaleY", "transition-duration":"0.5s"});
+        $(".catalog-element2").css({"line-height":"20px", "color":"white", "transform":"scaleY", "transition-duration":"0.5s", "transition-delay":".1s"});
+        $(".catalog-element3").css({"line-height":"20px", "color":"white", "transform":"scaleY", "transition-duration":"0.5s", "transition-delay":".2s"});
+        $(".catalog-element4").css({"line-height":"20px", "color":"white", "transform":"scaleY", "transition-duration":"0.5s", "transition-delay":".3s"});
+        })
+    
+     .mouseout(function(){
+        $(".catalog-element1").css({"line-height":"5px", "color":"transparent"});
+        $(".catalog-element2").css({"line-height":"5px", "color":"transparent", "transition-delay":".1s"});
+        $(".catalog-element3").css({"line-height":"5px", "color":"transparent", "transition-delay":".2s"});
+        $(".catalog-element4").css({"line-height":"5px", "color":"transparent", "transition-delay":".3s"});
+    })
+})
+
+$(document).ready(function(){
+    $(".catalog-element1")
+     .mouseover(function(){
+        $(".catalog-element1").css({"opacity":".3", "transition-duration":"0.5s"});
+    })
+    
+     .mouseout(function(){
+        $(".catalog-element1").css({"line-height":"5px", "opacity":".07", "color":"transparent"});
+    })
+})
+
+$(document).ready(function(){
+    $(".catalog-element2")
+     .mouseover(function(){
+        $(".catalog-element2").css({"opacity":".3", "transition-duration":"0.5s"});
+    })
+    
+     .mouseout(function(){
+        $(".catalog-element2").css({"line-height":"5px", "opacity":".07", "color":"transparent"});
+    })
+})
+
+$(document).ready(function(){
+    $(".catalog-element3")
+     .mouseover(function(){
+        $(".catalog-element3").css({"opacity":".3", "transition-duration":"0.5s"});
+    })
+    
+     .mouseout(function(){
+        $(".catalog-element3").css({"line-height":"5px", "opacity":".07", "color":"transparent"});
+    })
+})
+
+$(document).ready(function(){
+    $(".catalog-element4")
+     .mouseover(function(){
+        $(".catalog-element4").css({"opacity":".3", "transition-duration":"0.5s"});
+    })
+    
+     .mouseout(function(){
+        $(".catalog-element4").css({"line-height":"5px", "opacity":".07", "color":"transparent"});
+    })
+})
+
+
+$(document).ready(function(){
+    $(".slider").hover3d({
+        selector:".homeimg",
+        shine: true,
+        perspective: 3000,
+        sensitivity: 40,
+        invert:true,
+    })
+    
+    .hover3d({
+        selector:".project h1",
+        shine: true,
+        perspective: 3000,
+        sensitivity: 20,
+        invert:true,
+    })
+})
