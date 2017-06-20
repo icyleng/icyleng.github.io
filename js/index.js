73e88db5efe0de0697dfc242c7d3ab0fed93628c
icyleng <jleng@student.otis.edu>
2017-06-20 15:35:59 -0700

    frameset

--- js/index.js
+++ js/index.js
@@ -121,19 +121,24 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("item");
     var dots = document.getElementsByClassName("dot");
+    var text = document.getElementsByClassName("item-text");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
+    if (n > text.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = text.length}
+    
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i =0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "")
     }
+    for (i = 0; i < text.length; i++) {
+        text[i].style.display = "none";
+    }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className +=" active";
+    text[slideIndex-1].style.display = "block";
 }
-
-
-
 
 
